a921adf139582b2707f3c4eab8cf78a99b6d44d9
rm6462041-netizen <rm6462041@gmail.com>
2025-12-23 16:03:53 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,20 +1,9 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'db.ugqqqhdlzyymlrwqvwip.supabase.co', // IPv4 preference
-  database: 'postgres',
-  password: 'SbkvV5bYxYNP2Z0D',                 // your DB password
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
-
-pool
-  .query('SELECT 1')
-  .then(() => console.log('✅ DB CONNECTED SUCCESSFULLY'))
-  .catch((err) => console.error('❌ DB ERROR:', err.message));
 
 module.exports = pool;
